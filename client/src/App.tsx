2be091a2995787649a53a95d97e37b81212e0e53
yashwanth <yashwanthnapa@gmail.com>
2025-02-07 13:13:16 +0530

    initial

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>  
+      <div className="root-container">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
